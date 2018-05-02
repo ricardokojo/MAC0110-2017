5913356c5405f14480f8effd9880e3f07b6ceb93
Ricardo Hideki Hangai Kojo <ricardokojo7@gmail.com>
2018-05-02 18:42:29 -0300

    Adicionando EP7

---
+++ EP7 - Ex. 3 (Amigo secreto novamente)/EstimaPermutacoes.java
@@ -0,0 +1,61 @@
+﻿/******************************************************************************
+ *
+ *  MAC0110 - Introdução à Computação
+ *  Aluno: Ricardo Hideki Hangai Kojo
+ *  Numero USP: 10295429
+ *  Tarefa: Ex.3 Amigo Secreto (novamente)
+ *  Data: 11/04/2017
+ *  Baseado em:
+ *    Ex.2 Amigo Secreto e AmigoSecreto.java criado para tal exercício
+ *
+ ******************************************************************************/
+
+/******************************************************************************
+ *
+ * Nota: 9.75
+ *
+ * Comentários do Monitor:
+ *   [EstimaPermutacoes] Saída diferente da proposta no enunciado: -0.75;
+ *   Bônus (+0.5)
+ *
+ * Como rodar:
+ *   javac EstimaPermutacoes.java
+ *   java EstimaPermutacoes <nmax> <t>
+ * sendo <nmax> o número máximo de pessoas no amigo secreto e <t> a quantidade de permutações aleatórias geradas por número de pessoas.
+ *
+*******************************************************************************/
+
+public class EstimaPermutacoes{
+    public static void main(String[] args) {
+        int p_nmax = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+        
+        for(int i = 2; i<= p_nmax; i++){ //i e o numero de pessoas
+            int cont_boas = 0;
+            double porcentagem;
+            
+            for(int j = 1; j <= t; j++){ //j e o indice de testes
+                
+                //Gerador de permutacoes
+                int[] perm = new int[i];
+                int aux = 1;
+                int index_randomico;
+                while(aux <= i){
+                    index_randomico = (int)(Math.random() * i); //Index randomico de 0 até i atual
+                    if (perm[index_randomico] == 0){
+                        if (aux <= i && aux == index_randomico) break; // se i = a[i], sai;
+                        if(aux == i) {
+                            if (index_randomico != 0) cont_boas++;
+                            break;
+                        } //para aux = i, verifica index do 0
+                        perm[index_randomico] = aux;
+                        aux++;
+                    }
+                }
+            }
+            
+            porcentagem = (cont_boas * 100.0)/t;
+            System.out.println(i + " participantes> " + cont_boas + "/" + t + " ou " + porcentagem + "%");
+        }
+    }
+}
---
+++ EP7 - Ex. 3 (Amigo secreto novamente)/PermutacoesBoas.java
@@ -0,0 +1,67 @@
+﻿/******************************************************************************
+ *
+ *  MAC0110 - Introdução à Computação
+ *  Aluno: Ricardo Hideki Hangai Kojo
+ *  Numero USP: 10295429
+ *  Tarefa: Ex.3 Amigo Secreto (novamente) - Permutações Boas
+ *  Data: 11/04/2017
+ *  Baseado em:
+ *    Ex.2 Amigo Secreto e AmigoSecreto.java criado para tal exercício
+ *    Ex.3 Amigo Secreto (Novamente) e EstimaPermutacoes.
+ *
+ ******************************************************************************/
+
+/******************************************************************************
+ *
+ * Nota: 9.75
+ *
+ * Comentários do Monitor:
+ *   [EstimaPermutacoes] Saída diferente da proposta no enunciado: -0.75;
+ *   Bônus (+0.5)
+ *
+ * Como rodar:
+ *   javac PermutacoesBoas.java
+ *   java PermutacoesBoas <N>
+ *
+ * sendo <N> o número de elementos da permutação.
+ *
+*******************************************************************************/
+
+public class PermutacoesBoas {
+    public static void main(String[] args) {
+        int n = 4;
+        
+        int[] perm = new int[n];
+        boolean perm_ok = false;
+        int aux = 1;
+        int index_randomico;
+        
+        //Gerador de permutacoes
+        while(perm_ok == false){
+            while(aux <= n){
+                index_randomico = (int)(Math.random() * n); //Index randomico de 0 até i atual
+                if (perm[index_randomico] == 0){
+                    if (aux <= n && aux == index_randomico) break; // se i = a[i], sai;
+                    if(aux == n) {
+                        if (index_randomico != 0) perm_ok = true;
+                        break;
+                    } //para aux = i, verifica index do 0
+                    perm[index_randomico] = aux;
+                    aux++;
+                }
+            }
+            
+            if(perm_ok == false){ //se perm nao valida, cria outra
+                perm = new int[n];
+                aux = 1;
+            }
+        }
+        
+        // Print
+        String print = "";
+        for(int i = 0; i <= n - 1; i++){
+            print = print + perm[i] + " ";
+        }
+        System.out.println(print);
+    }
+}
